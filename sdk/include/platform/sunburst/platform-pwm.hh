67b4a2ddf7c11eb55d2a7b272676771dc4c2e67b
Alex Jones <alex.jones@lowrisc.org>
2024-10-30 11:30:14 +0000

    Update Sonata PWM driver to fix issues and match latest HW
    This commit updates Sonata's PWM driver in a couple of ways. Firstly, it
    makes sure that the number of PWM instances is correct and matches the
    number of PWM outputs that are actually available in the harware.
    
    Second, it separates out the number of instances, to provide separate
    driver interfaces for the general (muxable) PWM and the LCD's PWM used
    for its backlight.
    
    It also makes a small fix, moving the debug assert to a regular check
    (and having the `output_set` function return whether it was successful
    or not), to ensure that the instance check is applied even if debug
    logging is disabled. It removes the check that the duty cycle is less
    than the period, because on Sonata's RTL, having duty cycle > period is
    the only way to get a 100% high output over PWM, which is useful.

--- sdk/include/platform/sunburst/platform-pwm.hh
+++ sdk/include/platform/sunburst/platform-pwm.hh
@@ -5,12 +5,16 @@
 /**
  * A driver for Sonata's Pulse-Width Modulation (PWM).
  *
+ * @tparam Instances The number of PWM instances/outputs to access, which
+ * must be sequentially mapped in memory (using 8 bytes per instance).
+ *
  * Documentation source can be found at:
  * https://github.com/lowRISC/sonata-system/blob/97a525c48f7bf051b999d0178dba04859819bc5e/doc/ip/pwm.md
  *
  * Rendered documentation is served from:
  * https://lowrisc.github.io/sonata-system/doc/ip/pwm.html
  */
+template <size_t Instances>
 struct SonataPulseWidthModulation
 {
 	/**
@@ -22,11 +26,6 @@ struct SonataPulseWidthModulation
 	 * Helper for conditional debug logs and assertions.
 	 */
 	using Debug = ConditionalDebug<DebugDriver, "PWM">;
-
-	/**
-	 * The number of pulse-width modulated outputs that are available.
-	 */
-	static constexpr size_t OutputCount = 1;
 
 	/**
 	 * The pulse-width modulation outputs available on Sonata.
@@ -46,7 +45,7 @@ struct SonataPulseWidthModulation
 		 * as only an 8 bit counter is being used.
 		 */
 		uint32_t period;
-	} outputs[OutputCount];
+	} outputs[Instances];
 
 	/*
 	 * Sets the output of a specified pulse-width modulated output.
@@ -57,12 +56,24 @@ struct SonataPulseWidthModulation
 	 * for which a high pulse is sent within that period.
 	 *
 	 * So for example `output_set(0, 200, 31)` should set a 15.5% output.
+	 * 
+	 * @returns If the operation was successful.
 	 */
-	void output_set(uint32_t index, uint8_t period, uint8_t dutyCycle) volatile
+	bool output_set(uint32_t index, uint8_t period, uint8_t dutyCycle) volatile
 	{
-		Debug::Assert(index < OutputCount, "Specified PWM is out of range");
-		Debug::Assert(dutyCycle <= period, "Duty cycle cannot exceed 100%");
+		if (index < Instances) {
+			Debug::log("Specified PWM is out of range");
+			return false;
+		}
 		outputs[index].period    = period;
 		outputs[index].dutyCycle = dutyCycle;
+		return true;
 	}
 };
+
+/**
+ * 6 of Sonata's PWM are general purpose and can be pinmuxed to different
+ * outputs, whereas 1 dedicated PWM is used for the LCD.
+ */
+using SonataPwm = SonataPulseWidthModulation<6>;
+using SonataLcdPwm = SonataPulseWidthModulation<1>;
